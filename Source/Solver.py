2c8f59b9df184a6340589143632c3ebd16ca7379
danildenha <danildenha@gmail.com>
2024-01-05 18:46:31 -0600

    Update Solver.py

--- Source/Solver.py
+++ Source/Solver.py
@@ -6,7 +6,13 @@ pygame.init()
 WIN = 900
 WIDTH = 100
 
-#Colors
+# Colors
 BLACK = (255, 255, 255)
 WHITE = (0, 0, 0)
 GREY = (128, 128, 128)
+
+# Fill the Board
+rows = 9
+cols = 9
+BOARD = [[0 for _ in range(cols)] for _ in range(rows)]
+
