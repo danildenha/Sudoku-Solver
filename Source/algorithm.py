1efeeb9bef6b483250b6f58be4126c276804276d
danildenha <danildenha@gmail.com>
2024-01-10 21:19:25 -0600

    Update algorithm.py

--- Source/algorithm.py
+++ Source/algorithm.py
@@ -4,6 +4,12 @@ def is_valid(node, value, nodes, ROWS):
     for i in range(ROWS):
         if nodes[nodes.row][i].value == value and nodes[node.row][i] != node:
             return False
+        if nodes[i][nodes.col].value == value and nodes[i][nodes.col] != node:
+            return False
+        
+
+        
+    return True
 
 
 def algotithm(nodes, ROWS):
