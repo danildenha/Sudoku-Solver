5939f1880a29df91d5527f703c352e18ecf71a11
danildenha <danildenha@gmail.com>
2024-01-11 10:14:41 -0600

    Update algorithm.py

--- Source/algorithm.py
+++ Source/algorithm.py
@@ -1,16 +1,14 @@
 import pygame
 
-def is_valid(node, value, nodes, ROWS):
-    for i in range(ROWS):
-        if nodes[nodes.row][i].value == value and nodes[node.row][i] != node:
-            return False
-        if nodes[i][nodes.col].value == value and nodes[i][nodes.col] != node:
-            return False
-    
     #Check 3x3 box
+def isValidSudoku(self, board):
+        res = []
+        for i in range(9):
+                element = board[i][j]
+                if element != '.':
+                    res += [(i, element), (element, j), (i // 3, j // 3, element)]
+        return len(res) == len(set(res))
 
-
-    return True
 
 
 def algotithm(nodes, ROWS):
